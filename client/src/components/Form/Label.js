8e3ef784cac4bbe354f46b8b28b27af21aaa5d4c
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-17 17:41:54 +0700

    refactor(ui): update form label component

--- client/src/components/Form/Label.js
+++ client/src/components/Form/Label.js
@@ -1,6 +1,6 @@
 const Label = ({ name, required }) => {
     return (
-        <label className="block text-xs font-semibold text-slate-700 uppercase tracking-wider mb-1.5 ml-0.5">
+        <label className="block text-xs font-semibold text-text-secondary uppercase tracking-wider mb-1.5 ml-0.5">
             {name}
             {required && <span className="text-rose-500 ml-1">*</span>}
         </label>
