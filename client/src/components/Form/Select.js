8532d70305b8069f9d5d2e81f9a8e31e05db6e43
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-17 17:20:59 +0700

    refactor(ui): update form select component

--- client/src/components/Form/Select.js
+++ client/src/components/Form/Select.js
@@ -1,15 +1,16 @@
-const Select = ({ name, value, onChange, className, disabled, children }) => {
+const Select = ({ name, value, onChange, className = "", disabled, children, ...props }) => {
     return (
         <select
             name={name}
             value={value}
             onChange={onChange}
-            className={`w-full pl-9 p-2.5 border rounded-lg text-sm text-slate-700 ${className}`}
+            className={`w-full text-slate-800 bg-white px-3.5 py-2.5 border border-slate-300/80 rounded-xl focus:ring-4 focus:ring-emerald-500/20 focus:border-emerald-500 outline-none transition-all text-sm shadow-xs disabled:bg-slate-100 disabled:cursor-not-allowed ${className}`}
             disabled={disabled}
+            {...props}
         >
             {children}
         </select>
-    )
-}
+    );
+};
 
-export default Select
+export default Select;
