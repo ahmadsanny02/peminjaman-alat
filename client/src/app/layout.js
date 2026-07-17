8092896369a057f829c42f91a4b20630e084ce50
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-17 17:41:54 +0700

    refactor(layout): update app layout shell

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -19,6 +19,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                }
+              } catch (_) {}
+            `
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
